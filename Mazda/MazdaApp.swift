753bbd5fff52e7df4f0222b1a5094a3392821380
GG <rg.dev@outlook.com>
2023-12-12 21:55:15 +0800

    Initial Commit

---
+++ Mazda/MazdaApp.swift
@@ -0,0 +1,17 @@
+//
+//  MazdaApp.swift
+//  Mazda
+//
+//  Created by GG on 2023/12/12.
+//
+
+import SwiftUI
+
+@main
+struct MazdaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
